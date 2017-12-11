e0266e7bb3b6266cc0c33c6a7ea36250e0f82ec4
Princess Ellen Alto <princessellen0016@yahoo.com>
2017-12-11 18:32:17 +0800

    layouts

--- themes/default/views/pages/press.blade.php
+++ themes/default/views/pages/press.blade.php
@@ -20,6 +20,21 @@
         </v-parallax>
     </v-card>
 
+    <section id="mv" class="py-3">
+        <v-container fluid grid-list-lg>
+            <v-layout row wrap justify-center align-top>
+                <v-flex lg8 xs12>
+                    <v-card-text class="text-xs-center my-3">
+                        <h2 class="display-1">{{ __("WE ARE ABOUT UNIQUE ADVENTURES") }}</h2>
+                        <h2 class="subheading grey--text text--darken-1">
+                            Far far away, behind the word mountains, far from the countries Vokalia and Consonantia
+                        </h2>
+                    </v-card-text>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </section>
+
     <v-container fluid grid-list-lg>
         <v-layout row wrap align-center justify-center>
             <v-flex lg8 sm12 xs12>
